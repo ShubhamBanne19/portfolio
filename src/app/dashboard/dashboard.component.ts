5e16263fe130c75147ad0aa6c50eb6dca16a2df4
ShubhamBanne19 <shubhambanne1819@gmail.com>
2025-01-09 11:40:21 -0800

    Dashboard initial design v1

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  EmailID: string = "shubhambanne1819@gmail.com";
+  phoneNo: string ="+91 8999661297 ";
+  location: string = "Pune, India";
+  linkedInProfile: string ="https://www.linkedin.com/in/shubham-banne77981297271969780123";
+  gitHubProfile: string = "https://github.com/ShubhamBanne19";
+}
