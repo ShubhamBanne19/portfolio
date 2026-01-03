882b285c77113d99915e10e4933614140867d4c6
ShubhamBanne19 <shubhambanne1819@gmail.com>
2026-01-03 12:51:15 +0530

    hashing added in routing file

--- portfolioWork/src/app/app-routing.module.ts
+++ portfolioWork/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { CaseStudyComponent } from './pages/case-study/case-study.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  
   {path: 'project/:id', component: CaseStudyComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
